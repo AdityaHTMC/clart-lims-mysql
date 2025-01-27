1ee82b805468098f8a456c8d6d049dfcb0f05d79
Harsh Singh <harsh@indiaadmart.com>
2025-01-27 10:49:02 +0530

    axios instance updated

--- src/helper/axios-instance.js
+++ src/helper/axios-instance.js
@@ -13,7 +13,7 @@ axios.interceptors.response.use(
         // Check for 401 status code
         if (error.response && error.response.status === 401 && !href.includes('/login')) {
             // alert('Session expired. Please log in again.');
-            window.location.href = '/#/login' // Reload the page or redirect to login
+            window.location.href = '/admin/#/login' // Reload the page or redirect to login
         }
 
         // Reject the error so it can still be handled locally if needed
